56b0bb42e5b90b3b99433ea3ca4a495de4e5d490
tomaspasto24 <tomaspasto24@gmail.com>
2021-09-02 20:17:53 -0300

    Clases Agregadas

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -4,25 +4,22 @@ namespace SRP
 {
     public class Libro
     {
-
-        public string Titulo { get ; }
-        public string Autor { get ; }
-        public string Codigo { get ;  }
-        public string SectorBiblioteca { get ; set; }
-        public string EstanteBiblioteca { get ; set; }
+        public string Titulo { get ; private set; }
+        public string Autor { get ; private set; }
+        public string Codigo { get ; private set; }
+        public Ubicacion UbicacionLibro { get; private set; }
 
         public Libro(String titulo, String autor, String codigo)
         {
             this.Titulo = titulo;
             this.Autor = autor;
             this.Codigo = codigo;
+            
         }
 
         public void AlmacenarLibro(String sector, String estante)
         {
-            this.SectorBiblioteca = sector;
-            this.EstanteBiblioteca = estante;
+            this.UbicacionLibro = new Ubicacion(this, sector, estante);
         }
-
     }
 }
